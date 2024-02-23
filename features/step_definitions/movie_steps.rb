31b09f4298678307d53127d261ca1282b997a35a
hearT555 <98440480+hearT555@users.noreply.github.com>
2024-02-23 12:56:58 +0700

    Update movie_steps.rb

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(movie) #adding for create movie
   end
-  pending "Fill in this step in movie_steps.rb"
+  #pending "Fill in this step in movie_steps.rb" """comment to close pending"""
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -18,7 +19,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  pending "Fill in this step in movie_steps.rb"
+  #pending "Fill in this step in movie_steps.rb" """comment to close pending"""
+  expect(page.body.index(e1) < page.body.index(e2)).to be true #adding for no pending
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -29,18 +31,30 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  pending "Fill in this step in movie_steps.rb"
+  #pending "Fill in this step in movie_steps.rb"  """comment to close pending"""
+  rating_list.split(",").each do |rating| #adding for rating the movie
+    steps %{Then I #{uncheck}check "ratings_#{rating}"}
+  end
 end
 
 # Part 2, Step 3
 Then /^I should (not )?see the following movies: (.*)$/ do |no, movie_list|
   # Take a look at web_steps.rb Then /^(?:|I )should see "([^"]*)"$/
-  pending "Fill in this step in movie_steps.rb"
+  #pending "Fill in this step in movie_steps.rb" """comment to close pending"""
+  movie_list.split(",").each do |movie|
+    steps %{Then I should #{no}see "#{movie}"}
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  pending "Fill in this step in movie_steps.rb"
+  #pending "Fill in this step in movie_steps.rb" """comment to close pending"""
+  #Movie_all = Movie.select(:title)
+  temp = Movie.all #adding for see all movies
+  temp.each do |mov|
+    m = mov.title
+    steps %{Then I should see "#{m}"}
+  end
 end
 
 ### Utility Steps Just for this assignment.
